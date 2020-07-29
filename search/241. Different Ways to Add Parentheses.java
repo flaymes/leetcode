5a591e866efb33e22cd9e4fb5fe2ced49711d3d1
zengpeng2018 <zengpeng2018@gmail.com>
2020-07-29 22:53:04 +0800

    241

---
+++ search/241. Different Ways to Add Parentheses.java
@@ -0,0 +1,34 @@
+/**
+ * 分治法
+ * https://leetcode-cn.com/problems/different-ways-to-add-parentheses/
+ */
+class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> result=new ArrayList<>();
+        for(int i=0;i<input.length();i++){
+            char ch = input.charAt(i);
+            if( ch == '-' || ch == '+' || ch == '*'){
+                List<Integer> left = diffWaysToCompute(input.substring(0,i));
+                List<Integer> right = diffWaysToCompute(input.substring(i+1));
+
+                // 循环的原因是 有left、right有多种运算结果
+                for(int x: left){
+                    for(int y: right){
+                        if(ch=='-'){
+                            result.add(x-y);
+                        }else if(ch == '+'){
+                            result.add(x+y);
+                        }else{
+                            result.add(x*y);
+                        }
+                    }
+                }
+            }
+        }
+
+        if(result.size()==0){
+            result.add(Integer.parseInt(input));
+        }
+        return result;
+    }
+}

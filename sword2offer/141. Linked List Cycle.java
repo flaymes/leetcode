a6afb425d80e375bb7fcc3783679f7cec1dfe14d
zengpeng2018 <zengpeng2018@gmail.com>
2020-07-31 00:40:09 +0800

    141

---
+++ sword2offer/141. Linked List Cycle.java
@@ -0,0 +1,30 @@
+package sword2offer;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode p = head.next.next;
+        ListNode q = head;
+        while (p != null && p.next != null) {
+            if (p == q) {
+                return true;
+            }
+            p = p.next.next;
+            q = q.next;
+        }
+        return false;
+    }
+}

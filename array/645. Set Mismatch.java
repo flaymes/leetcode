2b7787c714aa68de3edcd0a0f1d57f9417345811
zengpeng2018 <zengpeng2018@gmail.com>
2020-09-19 10:13:51 +0800

    645

---
+++ array/645. Set Mismatch.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode-cn.com/problems/set-mismatch/
+ */
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int dup=-1;
+        int loss = -1;
+        for(int i=0;i<nums.length;i++){
+            int idx=Math.abs(nums[i])-1;
+            if(nums[idx]<0){
+                dup=Math.abs(nums[i]);
+            }else{
+                nums[idx]=nums[idx]*(-1);
+            }            
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                loss=i;
+            }
+        }
+
+        return new int[]{dup,loss+1};
+    }
